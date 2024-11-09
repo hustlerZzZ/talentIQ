6c715acca78a5a88b30d0c258b84d7d5c5baa9fd
Cygnusfx <jatinanand715@gmail.com>
2024-11-09 22:18:48 +0530

    minor changes

--- frontend/src/components/Notification.tsx
+++ frontend/src/components/Notification.tsx
@@ -4,7 +4,7 @@ function Notification() {
   return (
     <>
       <button
-        className={`px-5 py-3 bg-zinc-700 flex justify-center items-center rounded-full`}
+        className={`px-3 py-3 bg-zinc-800 flex justify-center items-center rounded-full`}
       >
         <FaBell className={`text-white text-xl rounded-full`} />
       </button>
--- frontend/src/components/Setting.tsx
+++ frontend/src/components/Setting.tsx
@@ -4,7 +4,7 @@ import { IoMdSettings } from "react-icons/io";
 function Setting() {
   return (
     <>
-        <button className="px-5 py-3 bg-zinc-800 rounded-full flex justify-center items-center">
+        <button className="px-3 py-3 bg-zinc-800 rounded-full flex justify-center items-center">
             <IoMdSettings className="text-white text-xl" />
           </button>
     </>
