c1c88a8ec2b2a2a8649069335d28d2d65be58e3e
Harshit <hrpofficial736@gmail.com>
2024-11-09 17:01:24 +0530

    initialised socket

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Interviewer from "./pages/Interviewer Dashboard/interviewer";
 import Interviewee from "./pages/Interviewee Dashboard/Interviewee";
 import Home from "./pages/Landing/Home";
+import ChatContainer from "./components/ChatContainer";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ function App() {
 
         <Route path="/interviewer" element={<Interviewer />} />
         <Route path="/interviewee" element={<Interviewee />} />
+        <Route path="/chatbot" element={<ChatContainer />} />
       </Routes>
     </BrowserRouter>
   );
