30197ce4ed72693054f8f50ade3b89a318550ffa
hustlerZzZ <rishabpal2000@gmail.com>
2024-11-09 22:14:47 +0530

    Added redux provider to main app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/index.css";
+
 import { ClerkProvider } from "@clerk/clerk-react";
+import { store } from "./redux/store.ts";
+import { Provider } from "react-redux";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -13,7 +16,9 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ClerkProvider>
   </StrictMode>,
 );
