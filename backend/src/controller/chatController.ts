dc61a8a3e26b5b5379eff8fa539fbdb72c37f36d
hustlerZzZ <rishabpal2000@gmail.com>
2024-11-09 20:26:09 +0530

    Added the interviewer web app

--- backend/src/controller/chatController.ts
+++ backend/src/controller/chatController.ts
@@ -1,23 +1,23 @@
-// import { Server, Socket } from "socket.io";
-// import http from 'node:http';
+import { Server, Socket } from "socket.io";
+import http from "node:http";
 
-// const server = http.createServer();
+const server = http.createServer();
 
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:5173",
-//     methods: ["GET", "POST"],
-//   },
-// });
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
-// io.on("message", (socket: Socket) => {
-//   console.log("A user connected:", socket.id);
-//   socket.on("message", (message) => {
-//     console.log("Received message:", message);
-//     io.emit("message", message);
-//   });
+io.on("message", (socket: Socket) => {
+  console.log("A user connected:", socket.id);
+  socket.on("message", (message) => {
+    console.log("Received message:", message);
+    io.emit("message", message);
+  });
 
-//   socket.on("disconnect", () => {
-//     console.log("User disconnected:", socket.id);
-//   });
-// });
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import InterviewerDashboard from "./pages/Interviewer-dashboard/Interviewer-dashboard.tsx";
 import Interviewee from "./pages/interviewee-app/Interviewee";
+import Interviewer from "./pages/interviewer-app/interviewer.tsx";
 import Home from "./pages/Landing/Home";
 import Pricing from "./pages/Pricing/Pricing";
 import ChatContainer from "./components/ChatContainer";
@@ -19,12 +20,16 @@ function App() {
             </SignedIn>
           }
         />
-        <Route path="/interviewee" element={<Interviewee />} />
-        <Route path="/pricing" element={<Pricing />} />
         <Route
-          path="/interviewer-dashboard"
-          element={<InterviewerDashboard />}
+          path="/interviewer-app"
+          element={
+            <SignedIn>
+              <Interviewer />
+            </SignedIn>
+          }
         />
+        <Route path="/interviewee-app" element={<Interviewee />} />
+        <Route path="/pricing" element={<Pricing />} />
         <Route path="/chatbot" element={<ChatContainer />} />
       </Routes>
     </BrowserRouter>
